d7f455c95ec07fd657a80570b62f2e594ff13d51
Benoît Eveillard <benoit.eveillard@gmail.com>
2021-09-18 08:55:03 -0400

    ATM

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,89 @@
-import { systemUnderTest } from ".";
+import {
+  withdrawWad,
+  withdraw,
+} from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("Test of withdrawWad", function () {
+
+  it("Should return 1 x 500 bill and rest 50", function () {
+    expect(withdrawWad(500, 550)).toEqual(
+      {
+        wads: [{ billValue: 500, nbBills: 1 }],
+        rest: 50,
+      }
+    );
   });
+
+});
+
+describe("Test of withdraw", function () {
+
+  it("Should return 1 x 500 bill", function () {
+    expect(withdraw(500)).toEqual(
+      {
+        wads:
+          [
+            { billValue: 500, nbBills: 1 },
+            { billValue: 200, nbBills: 0 },
+            { billValue: 100, nbBills: 0 },
+            { billValue: 50, nbBills: 0 },
+            { billValue: 20, nbBills: 0 },
+            { billValue: 10, nbBills: 0 },
+          ],
+        rest: 0
+      }
+    );
+  });
+
+  it("Should return 1 x 10 bill", function () {
+    expect(withdraw(10)).toEqual(
+      {
+        wads:
+          [
+            { billValue: 500, nbBills: 0 },
+            { billValue: 200, nbBills: 0 },
+            { billValue: 100, nbBills: 0 },
+            { billValue: 50, nbBills: 0 },
+            { billValue: 20, nbBills: 0 },
+            { billValue: 10, nbBills: 1 },
+          ],
+        rest: 0
+      }
+    );
+  });
+
+  it("Should return 1 x 20 + 1 x 10 bills", function () {
+    expect(withdraw(30)).toEqual(
+      {
+        wads:
+          [
+            { billValue: 500, nbBills: 0 },
+            { billValue: 200, nbBills: 0 },
+            { billValue: 100, nbBills: 0 },
+            { billValue: 50, nbBills: 0 },
+            { billValue: 20, nbBills: 1 },
+            { billValue: 10, nbBills: 1 },
+          ],
+        rest: 0
+      }
+    );
+  });
+
+  it("Should return 1 x 200 + 1 x 50 + 1 x 20 bills", function () {
+    expect(withdraw(270)).toEqual(
+      {
+        wads:
+          [
+            { billValue: 500, nbBills: 0 },
+            { billValue: 200, nbBills: 1 },
+            { billValue: 100, nbBills: 0 },
+            { billValue: 50, nbBills: 1 },
+            { billValue: 20, nbBills: 1 },
+            { billValue: 10, nbBills: 0 },
+          ],
+        rest: 0
+      }
+    );
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-export function systemUnderTest(): boolean {
-  return true;
+const BILL_VALUES: number[] = [500, 200, 100, 50, 20, 10];
+
+class Wad {
+  billValue: number;
+  nbBills: number;
+}
+
+class Withdrawal {
+  wads: Wad[];
+  rest: number;
+}
+
+export function withdraw(amount: number): Withdrawal {
+
+  function withdrawNextBillValue({ wads: withdrawals, rest }: Withdrawal, billValue: number): Withdrawal {
+    const withdrawal: Withdrawal = withdrawWad(billValue, rest);
+
+    return {
+      wads: [...withdrawals, ...withdrawal.wads],
+      rest: withdrawal.rest,
+    };
+  }
+
+  return BILL_VALUES.reduce(withdrawNextBillValue, { wads: [], rest: amount });
+}
+
+export function withdrawWad(billValue: number, amount: number): Withdrawal {
+  const rest: number = amount % billValue;
+  const nbBills: number = (amount - rest) / billValue;
+
+  return { wads: [{ billValue: billValue, nbBills }], rest };
 }
